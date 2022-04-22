bb6d6d437b210bc7c65e99892bccb1976910e831
Dattesh222 <dattesh2@gmail.com>
2022-04-22 18:38:16 +0530

    i inserted into a function

--- index.py
+++ index.py
@@ -1,4 +1,7 @@
-first_name='dattesh'
-second_name='alugu'
-third_name='vasanth'
-fourth_name='mumbai'
+def new_func():
+    first_name='dattesh'
+    second_name='alugu'
+    third_name='vasanth'
+    fourth_name='mumbai'
+
+new_func()
